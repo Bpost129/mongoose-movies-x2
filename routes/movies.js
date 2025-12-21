bf22918eb37a9d47dad14e4324eb91acc0439105
Bpost129 <Bpost129@yahoo.com>
2025-12-21 13:45:22 -0500

    install and import dotenv, set up database connection, refactor server to show resource change, rename router file

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 const router = Router()
 
-// GET localhost:3000/users
+// GET localhost:3000/movies
 router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
