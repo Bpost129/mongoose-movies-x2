b052a639683806995426dec298817113f757a246
Bpost129 <Bpost129@yahoo.com>
2025-12-30 11:13:29 -0500

    create post route and controller function for performers, add performer model reference to movie model

--- controllers/performers.js
+++ controllers/performers.js
@@ -14,7 +14,15 @@ function newPerformer(req, res) {
   })
 }
 
+function create(req, res) {
+  Performer.create(req.body)
+  .then(performer => {
+    res.redirect('/performers/new')
+  })
+}
+
 export {
   newPerformer as new,
+  create,
 
 }
--- routes/performers.js
+++ routes/performers.js
@@ -4,5 +4,6 @@ import * as performersCtrl from '../controllers/performers.js'
 const router = Router()
 
 router.get('/new', performersCtrl.new)
+router.post('/', performersCtrl.create)
 
 export { router }
