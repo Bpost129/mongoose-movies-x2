77ac4804cf94e91d29c1d1de43d9999aaacb0949
Bpost129 <Bpost129@yahoo.com>
2025-12-31 13:40:42 -0500

    add birth date to performer model and new form

--- models/performer.js
+++ models/performer.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const performerSchema = new Schema({
-  name: {type: String, required: true}
+  name: {type: String, required: true},
+  born: Date,
 }, {
   timestamps: true
 })
